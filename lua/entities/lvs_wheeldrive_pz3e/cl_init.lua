1c0f6cfc12b585775edf0dcf40c3b2280e329c8b
Luna <swagnemite@gmx.de>
2023-10-05 21:14:32 +0200

    prevent first person for commander

--- lua/entities/lvs_wheeldrive_pz3e/cl_init.lua
+++ lua/entities/lvs_wheeldrive_pz3e/cl_init.lua
@@ -4,3 +4,11 @@ ENT.TrackLeftSubMaterialID = 7
 ENT.TrackRightSubMaterialID = 8
 
 ENT.OpticsProjectileSize = 3.7
+
+function ENT:TankGunnerViewOverride( ply, pos, angles, fov, pod )
+	if pod == self:GetTopGunnerSeat() and not pod:GetThirdPersonMode() then
+		pod:SetThirdPersonMode( true )
+	end
+
+	return pos, angles, fov
+end
