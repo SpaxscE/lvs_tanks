b7e03bf6ec597f9ca78410889fa82e1e41184ac3
Luna <swagnemite@gmx.de>
2023-10-07 17:39:29 +0200

    added wespe, removed unused models

---
+++ lua/entities/lvs_wheeldrive_wespe/shared.lua
@@ -0,0 +1,122 @@
+
+ENT.Base = "lvs_tank_wheeldrive"
+
+ENT.PrintName = "Wespe"
+ENT.Author = "Luna"
+ENT.Information = "Luna's Vehicle Script"
+ENT.Category = "[LVS] - Cars"
+
+ENT.Spawnable			= true
+ENT.AdminSpawnable		= false
+
+ENT.MDL = "models/diggercars/pz2/wespe.mdl"
+
+ENT.AITEAM = 1
+
+ENT.MaxHealth = 800
+
+--damage system
+ENT.DSArmorIgnoreForce = 1000
+
+ENT.SteerSpeed = 1
+ENT.SteerReturnSpeed = 2
+
+ENT.PhysicsWeightScale = 2
+ENT.PhysicsDampingSpeed = 1000
+ENT.PhysicsInertia = Vector(6000,6000,1500)
+
+ENT.MaxVelocity = 450
+ENT.MaxVelocityReverse = 150
+
+ENT.EngineCurve = 0.1
+ENT.EngineTorque = 300
+
+ENT.TransMinGearHoldTime = 0.1
+ENT.TransShiftSpeed = 0
+
+ENT.TransGears = 3
+ENT.TransGearsReverse = 1
+
+ENT.MouseSteerAngle = 45
+
+ENT.WheelBrakeAutoLockup = true
+ENT.WheelBrakeLockupRPM = 15
+
+ENT.lvsShowInSpawner = true
+
+ENT.EngineSounds = {
+	{
+		sound = "lvs/vehicles/pz2/eng_idle_loop.wav",
+		Volume = 1,
+		Pitch = 70,
+		PitchMul = 30,
+		SoundLevel = 75,
+		SoundType = LVS.SOUNDTYPE_IDLE_ONLY,
+	},
+	{
+		sound = "lvs/vehicles/pz2/eng_loop.wav",
+		Volume = 1,
+		Pitch = 20,
+		PitchMul = 100,
+		SoundLevel = 85,
+		SoundType = LVS.SOUNDTYPE_NONE,
+		UseDoppler = true,
+	}
+}
+
+ENT.ExhaustPositions = {
+	{
+		pos = Vector(-110,-28.7,45.8),
+		ang = Angle(-130,0,0)
+	},
+}
+
+function ENT:InitWeapons()
+	local weapon = {}
+	weapon.Icon = Material("lvs/weapons/bomb.png")
+	weapon.Ammo = 32
+	weapon.Delay = 4
+	weapon.HeatRateUp = 0
+	weapon.HeatRateDown = 0.25
+	weapon.StartAttack = function( ent )
+	
+		if self:GetAI() then return end
+
+		self:MakeProjectile()
+	end
+	weapon.FinishAttack = function( ent )
+		if self:GetAI() then return end
+
+		self:FireProjectile()
+	end
+	weapon.Attack = function( ent )
+		if not self:GetAI() then return end
+
+		self:MakeProjectile()
+		self:FireProjectile()
+	end
+	weapon.OnThink = function( ent, active )
+		if ent:GetSelectedWeapon() ~= 1 then return end
+
+		local Angles = ent:WorldToLocalAngles( ent:GetAimVector():Angle() )
+		Angles:Normalize()
+
+		ent:SetPoseParameter("turret_yaw", -Angles.y )
+		ent:SetPoseParameter("turret_pitch", -Angles.p )
+	end
+	weapon.HudPaint = function( ent, X, Y, ply )
+		local Pos2D = ent:GetEyeTrace().HitPos:ToScreen()
+
+		ent:LVSPaintHitMarker( Pos2D )
+	end
+	self:AddWeapon( weapon )
+
+	-- turret rotation disabler
+	local weapon = {}
+	weapon.Icon = Material("lvs/weapons/tank_noturret.png")
+	weapon.Ammo = -1
+	weapon.Delay = 0
+	weapon.HeatRateUp = 0
+	weapon.HeatRateDown = 0
+	self:AddWeapon( weapon )
+end
