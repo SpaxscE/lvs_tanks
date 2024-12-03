82870c5978060f830cd3c5938256e9eb214b891d
Luna <24762027+SpaxscE@users.noreply.github.com>
2024-12-03 17:05:34 +0100

    update pz2 scope

--- lua/entities/lvs_wheeldrive_pz2c/sh_turret.lua
+++ lua/entities/lvs_wheeldrive_pz2c/sh_turret.lua
@@ -1,5 +1,10 @@
 
 include("entities/lvs_tank_wheeldrive/modules/sh_turret.lua")
+include("entities/lvs_tank_wheeldrive/modules/sh_turret_ballistics.lua")
+
+ENT.TurretBallisticsProjectileVelocity = ENT.ProjectileVelocityCoaxial
+ENT.TurretBallisticsMuzzleAttachment = "muzzle_3"
+ENT.TurretBallisticsViewAttachment = "muzzle_4"
 
 ENT.TurretAimRate = 25
 
@@ -14,3 +19,15 @@ ENT.TurretPitchOffset = 0
 ENT.TurretYawPoseParameterName = "turret_yaw"
 ENT.TurretYawMul = 1
 ENT.TurretYawOffset = 0
+
+function ENT:GetTurretViewOrigin()
+	local ID = self:LookupAttachment( self.TurretBallisticsViewAttachment )
+
+	local Att = self:GetAttachment( ID )
+
+	if not Att then return self:GetPos(), false end
+
+	local Pos = Att.Pos + Att.Ang:Up() * 5 - Att.Ang:Forward() * 32 - Att.Ang:Right() * 5
+
+	return Pos, true
+end
