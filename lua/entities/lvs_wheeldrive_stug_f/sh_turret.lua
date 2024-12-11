5e3e7d864547b5959047b5735a89934ed58819bd
Diggergithub <diggerddat@yandex.ru>
2024-12-11 15:43:32 +0500

    pew pew

---
+++ lua/entities/lvs_wheeldrive_stug_f/sh_turret.lua
@@ -0,0 +1,35 @@
+
+include("entities/lvs_tank_wheeldrive/modules/sh_turret.lua")
+include("entities/lvs_tank_wheeldrive/modules/sh_turret_ballistics.lua")
+
+ENT.TurretBallisticsProjectileVelocity = ENT.ProjectileVelocityArmorPiercing
+ENT.TurretBallisticsMuzzleAttachment = "muzzle"
+ENT.TurretBallisticsViewAttachment = "sight"
+
+ENT.TurretAimRate = 25
+
+ENT.TurretRotationSound = "vehicles/tank_turret_loop1.wav"
+
+ENT.TurretPitchPoseParameterName = "turret_pitch"
+ENT.TurretPitchMin = -20
+ENT.TurretPitchMax = 10
+ENT.TurretPitchMul = -1
+ENT.TurretPitchOffset = 0
+
+ENT.TurretYawPoseParameterName = "turret_yaw"
+ENT.TurretYawMin = -14
+ENT.TurretYawMax = 14
+ENT.TurretYawMul = -1
+ENT.TurretYawOffset = 0
+
+function ENT:GetTurretViewOrigin()
+	local ID = self:LookupAttachment( self.TurretBallisticsViewAttachment )
+
+	local Att = self:GetAttachment( ID )
+
+	if not Att then return self:GetPos(), false end
+
+	local Pos = Att.Pos - Att.Ang:Forward() * 30 - Att.Ang:Right() * 3 - Att.Ang:Up() * 10
+
+	return Pos, true
+end
