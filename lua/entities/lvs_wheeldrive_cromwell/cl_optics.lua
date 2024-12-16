6927d825e9357a00ce7da9ff0227e6d47e0a0cfa
Luna <24762027+SpaxscE@users.noreply.github.com>
2024-12-16 13:37:16 +0100

    sight

--- lua/entities/lvs_wheeldrive_cromwell/cl_optics.lua
+++ lua/entities/lvs_wheeldrive_cromwell/cl_optics.lua
@@ -1,5 +1,5 @@
 
-ENT.OpticsFov = 20
+ENT.OpticsFov = 30
 ENT.OpticsEnable = true
 ENT.OpticsZoomOnly = true
 ENT.OpticsFirstPerson = true
@@ -8,48 +8,63 @@ ENT.OpticsPodIndex = {
 	[1] = true,
 }
 
-local scope = Material( "lvs/sight_empty.png" )
-local aim = Material( "lvs/28pdr_aim.png" )
-local rot = Material( "lvs/28pdr_sight.png" )
+ENT.OpticsCrosshairMaterial = Material( "lvs/circle_filled.png" )
+ENT.OpticsCrosshairColor = Color(0,0,0,150)
+ENT.OpticsCrosshairSize = 4
+
+local axis = Material( "lvs/28pdr_aim.png" )
+local sight = Material( "lvs/28pdr_sight.png" )
+local scope = Material( "lvs/scope_viewblocked.png" )
+
+function ENT:PaintOpticsCrosshair( Pos2D )
+	local ScrW = ScrW()
+	local ScrH = ScrH()
+
+	local ScopeSize = ScrH
+
+	surface.SetDrawColor( 0, 0, 0, 255 )
+
+	surface.SetMaterial( axis )
+	surface.DrawTexturedRect( Pos2D.x - ScopeSize * 0.5, Pos2D.y - ScopeSize * 0.5, ScopeSize, ScopeSize )
+
+	surface.DrawLine( Pos2D.x - ScopeSize * 0.19, Pos2D.y, 0, Pos2D.y )
+	surface.DrawLine( Pos2D.x + ScopeSize * 0.19, Pos2D.y, ScrW, Pos2D.y )
+	surface.DrawLine( Pos2D.x - 1, Pos2D.y - ScopeSize * 0.21, Pos2D.x - 1, 0 )
+	surface.DrawLine( Pos2D.x - 1, Pos2D.y + ScopeSize * 0.16, Pos2D.x - 1, ScrH )
+end
 
 function ENT:PaintOptics( Pos2D, Col, PodIndex, Type )
+	local size = self.OpticsCrosshairSize
+
+	surface.SetMaterial( self.OpticsCrosshairMaterial )
+	surface.SetDrawColor( self.OpticsCrosshairColor )
+	surface.DrawTexturedRect( Pos2D.x - size * 0.5, Pos2D.y - size * 0.5, size, size )
+
 	if Type == 1 then
-		self:DrawRotatedText( "BESA", Pos2D.x + 30, Pos2D.y + 30, "LVS_FONT_PANEL", Color(0,0,0,220), 0)
+		self:DrawRotatedText( "BESA", Pos2D.x + 30, Pos2D.y + 10, "LVS_FONT_PANEL", Color(0,0,0,220), 0)
 	else
-		self:DrawRotatedText( Type == 3 and "HE" or "APC", Pos2D.x + 30, Pos2D.y + 30, "LVS_FONT_PANEL", Color(0,0,0,220), 0)
+		self:DrawRotatedText( Type == 3 and "HE" or "APC", Pos2D.x + 30, Pos2D.y + 10, "LVS_FONT_PANEL", Color(0,0,0,220), 0)
 	end
 
-	local Width = ScrW()
-	local Height = ScrH()
+	local ScrW = ScrW()
+	local ScrH = ScrH()
 
-	local ScopeSize = Height
+	local diameter = ScrH + 64
+	local radius = diameter * 0.5
 
 	local TurretBallisticsValue = self:GetTurretCompensation()
 
 	-- how much scope offsets
-	local Offset = (TurretBallisticsValue / Height) * 18
+	local Offset = (TurretBallisticsValue / ScrH) * 100 - radius * 0.15
 
-	-- changes default Y offset of 'rot'
-	local OffsetRotator = -ScopeSize * 0.04
+	surface.SetMaterial( sight )
+	surface.SetDrawColor( 0, 0, 0, 255 )
+	surface.DrawTexturedRect( Pos2D.x - radius, Pos2D.y - radius + Offset, diameter, diameter )
 
 	surface.SetMaterial( scope )
-	surface.SetDrawColor( 0, 0, 0, 255 )
-	surface.DrawTexturedRect( Pos2D.x - ScopeSize * 0.5, Pos2D.y - ScopeSize * 0.5 + Offset, ScopeSize, ScopeSize )
-
-	-- black bar top + bottom
-	surface.DrawRect( Pos2D.x - ScopeSize * 0.5, Pos2D.y - ScopeSize * 0.5 - ScopeSize + Offset, ScopeSize, ScopeSize )
-	surface.DrawRect( Pos2D.x - ScopeSize * 0.5, Pos2D.y - ScopeSize * 0.5 + ScopeSize + Offset, ScopeSize, ScopeSize )
+	surface.DrawTexturedRect( Pos2D.x - radius, Pos2D.y - radius, diameter, diameter )
 
 	-- black bar left + right
-	surface.DrawRect( 0, 0, Width * 0.5 - ScopeSize * 0.5, Height )
-	surface.DrawRect( Width * 0.5 + ScopeSize * 0.5, 0, Width * 0.5 - ScopeSize * 0.5, Height )
-
-	surface.SetMaterial( aim )
-	surface.DrawTexturedRect( Pos2D.x - ScopeSize * 0.5, Pos2D.y - ScopeSize * 0.5, ScopeSize, ScopeSize )
-
-	-- reduces 'rot' scaling
-	ScopeSize = ScopeSize * 0.85
-
-	surface.SetMaterial( rot )
-	surface.DrawTexturedRect( Pos2D.x - ScopeSize * 0.5, Pos2D.y - ScopeSize * 0.5 + Offset + OffsetRotator, ScopeSize, ScopeSize )
+	surface.DrawRect( 0, 0, Pos2D.x - radius, ScrH )
+	surface.DrawRect( Pos2D.x + radius, 0, Pos2D.x - radius, ScrH )
 end
