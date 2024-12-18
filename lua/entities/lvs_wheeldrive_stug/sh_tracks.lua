289df3034cf5f08b6c5a6b296e38f0e0debc28da
Diggergithub <diggerddat@yandex.ru>
2024-12-18 19:17:46 +0500

    stug tracks

--- lua/entities/lvs_wheeldrive_stug/sh_tracks.lua
+++ lua/entities/lvs_wheeldrive_stug/sh_tracks.lua
@@ -4,6 +4,23 @@ if SERVER then
 	ENT.PivotSteerByBrake = true
 	ENT.PivotSteerWheelRPM = 40
 
+	ENT.TrackGibs = {
+		["left"] = {
+			{
+				mdl = "models/diggercars/pz3/tracks_ragdoll.mdl",
+				pos = Vector(0,48.6,0),
+				ang = Angle(-90,-90,0),
+			},
+		},
+		["right"] = {
+			{
+				mdl = "models/diggercars/pz3/tracks_ragdoll.mdl",
+				pos = Vector(0,-48.6,0),
+				ang = Angle(-90,-90,0),
+			},
+		}
+	}
+
 	function ENT:OnLeftTrackRepaired()
 		self:SetBodygroup(2,0)
 	end
